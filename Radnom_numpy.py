068e978cd2d84ac8bee04cbef2ea4350546438da
angelmapmaker <angelmapmaker@gmail.com>
2019-12-20 00:14:57 +0000

    Update Radnom_numpy.py

--- Radnom_numpy.py
+++ Radnom_numpy.py
@@ -76,3 +76,64 @@ for x in range(100) :
 
 # Print random_walk
 print(random_walk)
+
+#How low can you go?
+#Things are shaping up nicely! You already have code that calculates your location in the Empire State Building after 100 dice throws. However, there's something we haven't thought about - you can't go below 0!
+#A typical way to solve problems like this is by using max(). If you pass max() two arguments, the biggest one gets returned. For example, to make sure that a variable x never goes below 10 when you decrease it, you can use:
+# x = max(10, x - 1)
+# Numpy is imported, seed is set
+import numpy as np
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0,step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+
+
+#Visualize the walk
+#Let's visualize this random walk! Remember how you could use matplotlib to build a line plot?
+#import matplotlib.pyplot as plt
+#plt.plot(x, y)
+#plt.show()
+#The first list you pass is mapped onto the x axis and the second list is mapped onto the y axis.
+#If you pass only one argument, Python will know what to do and will use the index of the list to map onto the x axis, and the values in the list onto the y axis.
+
+# Numpy is imported, seed is set
+
+# Initialization
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+plt.show()
+
+# Show the plot
