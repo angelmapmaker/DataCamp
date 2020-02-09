9ca87b47dd6031f773f8a6ff7d662852d863289b
angelmapmaker <angelmapmaker@gmail.com>
2020-02-09 13:41:57 -0600

    Update pandas_fundation.py

--- pandas_fundation.py
+++ pandas_fundation.py
@@ -31,3 +31,16 @@ list_labels = ['year','artist','song','char weeks']
 
 # Assign the list of labels to the columns attribute: df.columns
 df.columns = list_labels
+
+
+# Make a string with the value 'PA': state
+state = 'PA'
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
