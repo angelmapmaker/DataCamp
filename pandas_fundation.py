5693d5885adb7b287f5a834966e124964ada6d3f
angelmapmaker <angelmapmaker@gmail.com>
2020-02-09 21:51:43 -0600

    Update pandas_fundation.py

--- pandas_fundation.py
+++ pandas_fundation.py
@@ -107,3 +107,21 @@ plt.xlabel('Hours since midnight August 1, 2010')
 plt.ylabel('Temperature (degrees F)')
 
 plt.show()
+
+# Plot all columns (default)
+df.plot()
+plt.show()
+
+# Plot all columns as subplots
+df.plot(subplots=True)
+plt.show()
+
+# Plot just the Dew Point data
+column_list1 = ['Dew Point (deg F)']
+df[column_list1].plot()
+plt.show()
+
+# Plot the Dew Point and Temperature data, but not the Pressure data
+column_list2 = ['Temperature (deg F)','Dew Point (deg F)']
+df[column_list2].plot()
+plt.show()
