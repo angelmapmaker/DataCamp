1cbdef42c4be79f6ae50170183be1aac2149f2e4
angelmapmaker <angelmapmaker@gmail.com>
2020-02-11 00:39:55 +0000

    Update pandas_fundation_II.py

--- pandas_fundation_II.py
+++ pandas_fundation_II.py
@@ -63,3 +63,13 @@ mean.plot()
 
 # Display the plot
 plt.show()
+
+
+# Print summary statistics of the fare column with .describe()
+print(df.fare.describe())
+
+# Generate a box plot of the fare column
+df['fare'].plot(kind='box')
+
+# Show the plot
+plt.show()
