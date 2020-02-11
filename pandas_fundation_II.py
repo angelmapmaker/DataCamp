d67b7eb50293d42f24d865d6455949785b93a8ea
angelmapmaker <angelmapmaker@gmail.com>
2020-02-11 00:07:43 +0000

    Update pandas_fundation_II.py

--- pandas_fundation_II.py
+++ pandas_fundation_II.py
@@ -12,3 +12,18 @@ plt.ylabel('Price ($US)')
 
 # Display the plot
 plt.show()
+
+# Generate a scatter plot
+df.plot(kind='scatter', x='hp', y='mpg', s=sizes)
+
+# Add the title
+plt.title('Fuel efficiency vs Horse-power')
+
+# Add the x-axis label
+plt.xlabel('Horse-power')
+
+# Add the y-axis label
+plt.ylabel('Fuel efficiency (mpg)')
+
+# Display the plot
+plt.show()
