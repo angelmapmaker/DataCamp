3aff34c3675ad743f6a7ee697d785cf2563b1471
angelmapmaker <angelmapmaker@gmail.com>
2020-02-15 16:53:58 -0600

    Update pandas_fundation_III.py

--- pandas_fundation_III.py
+++ pandas_fundation_III.py
@@ -26,3 +26,22 @@ ts2 = ts0.loc['2010-07-04']
 
 # Extract data from '2010-12-15' to '2010-12-31': ts3
 ts3 = ts0.loc['2010-12-15':'2010-12-31']
+#################################################################
+#
+################################################################
+
+
+# Reindex without fill method: ts3
+ts3 = ts2.reindex(ts1.index)
+
+# Reindex with fill method, using forward fill: ts4
+ts4 = ts2.reindex(ts1.index,method="ffill")
+
+# Combine ts1 + ts2: sum12
+sum12 = ts1 + ts2
+
+# Combine ts1 + ts3: sum13
+sum13 =  ts1 + ts3
+
+# Combine ts1 + ts4: sum14
+sum14 = ts1 + ts4
