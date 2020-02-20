35f3f6b903bd4f6342dce21ac485e4d0016292ff
angelmapmaker <angelmapmaker@gmail.com>
2020-02-19 21:54:28 -0600

    Update pandas_fundation_case_study_IV.py

--- pandas_fundation_case_study_IV.py
+++ pandas_fundation_case_study_IV.py
@@ -26,6 +26,9 @@ print(df.head())
 df_headers = pd.read_csv(data_file, header=None)
 
 # Print the output of df_headers.head()
+
+
+
 print(df_headers.head())
 
 ###################################################################33
@@ -290,4 +293,27 @@ sunny_fraction = sunny_hours / total_hours
 
 # Make a box plot of sunny_fraction
 sunny_fraction.plot(kind='box')
+plt.show()
+
+
+################################################3
+#Heat or humidity
+#Dew point is a measure of relative humidity based on pressure and temperature. A dew point above 65 is considered uncomfortable while a temperature above 90 is also considered uncomfortable.
+#
+#In this exercise, you will explore the maximum temperature and dew point of each month. The columns of interest are 'dew_point_faren' and 'dry_bulb_faren'. After resampling them appropriately to get the maximum temperature and dew point in each month, generate a histogram of these values as subplots. Uncomfortably, you will notice that the maximum dew point is above 65 every month!
+#
+#df_clean has been pre-loaded for you.
+#
+#Instructions
+#100 XP
+#Select the 'dew_point_faren' and 'dry_bulb_faren' columns (in that order). Resample by month and aggregate the maximum monthly temperatures. Assign the result to monthly_max.
+#Plot a histogram of the resampled data with bins=8, alpha=0.5, and subplots=True.
+
+# Resample dew_point_faren and dry_bulb_faren by Month, aggregating the maximum values: monthly_max
+monthly_max = df_clean.loc[:,['dew_point_faren' , 'dry_bulb_faren']]
+
+# Generate a histogram with bins=8, alpha=0.5, subplots=True
+monthly_max.plot(kind='hist',bins=8, alpha=0.5, subplots=True)
+
+# Show the plot
 plt.show()
