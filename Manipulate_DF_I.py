7ebcc19b16bca5af444536220370840cb13f8790
angelmapmaker <angelmapmaker@gmail.com>
2020-02-21 21:16:08 -0600

    Update Manipulate_DF_I.py

--- Manipulate_DF_I.py
+++ Manipulate_DF_I.py
@@ -24,3 +24,15 @@ y = 4
 
 # Print the boolean equivalence
 print(election.iloc[x, y] == election.loc['Bedford', 'winner'])
+
+# Import pandas
+import pandas as pd
+
+# Read in filename and set the index: election
+election = pd.read_csv(filename, index_col='county')
+
+# Create a separate dataframe with the columns ['winner', 'total', 'voters']: results
+results = election[ ['winner', 'total', 'voters']]
+
+# Print the output of results.head()
+print(results.head())
