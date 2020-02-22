e6f04bc45e44a949db6c89d43b6dce1596e45130
angelmapmaker <angelmapmaker@gmail.com>
2020-02-21 21:31:49 -0600

    Update Manipulate_DF_I.py

--- Manipulate_DF_I.py
+++ Manipulate_DF_I.py
@@ -36,3 +36,17 @@ results = election[ ['winner', 'total', 'voters']]
 
 # Print the output of results.head()
 print(results.head())
+
+######################################
+
+# Slice the row labels 'Perry' to 'Potter': p_counties
+p_counties =election.loc['Perry' : 'Potter']
+
+# Print the p_counties DataFrame
+print(p_counties)
+
+# Slice the row labels 'Potter' to 'Perry' in reverse order: p_counties_rev
+p_counties_rev = election.loc['Potter':'Perry' :-1]
+
+# Print the p_counties_rev DataFrame
+print(p_counties_rev)
