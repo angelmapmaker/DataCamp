b46a56328d237b8b5bd8e400d8d1c973881b1d43
angelmapmaker <angelmapmaker@gmail.com>
2020-03-01 09:23:23 -0600

    Update Manipulate_DF_III.py

--- Manipulate_DF_III.py
+++ Manipulate_DF_III.py
@@ -103,3 +103,17 @@ print(newusers)
 
 # Verify that the new DataFrame is equal to the original
 print(newusers.equals(users))
+
+#############################################################3
+#################### M E L T###########################
+# Reset the index: visitors_by_city_weekday
+visitors_by_city_weekday = visitors_by_city_weekday.reset_index()
+
+# Print visitors_by_city_weekday
+print(visitors_by_city_weekday)
+
+# Melt visitors_by_city_weekday: visitors
+visitors = pd.melt(visitors_by_city_weekday, id_vars=['weekday'], value_name='visitors')
+
+# Print visitors
+print(visitors)
