9e4defdf364ce074cd3c0c82bee2be0042341b13
angelmapmaker <angelmapmaker@gmail.com>
2020-03-01 20:45:46 -0600

    Update Manipulate_DF_III.py

--- Manipulate_DF_III.py
+++ Manipulate_DF_III.py
@@ -143,3 +143,16 @@ by_city_day =  users.pivot_table(index='weekday', columns='city')
 # Print by_city_day
 print(by_city_day)
 
+#################################################################
+# Use a pivot table to display the count of each column: count_by_weekday1
+count_by_weekday1 = users.pivot_table(index='weekday',columns='city',aggfunc='count')
+
+# Print count_by_weekday
+print(count_by_weekday1)
+
+# Replace 'aggfunc='count'' with 'aggfunc=len': count_by_weekday2
+count_by_weekday2 = users.pivot_table(index='weekday',columns='city',aggfunc=len)
+
+# Verify that the same result is obtained
+print('==========================================')
+print(count_by_weekday1.equals(count_by_weekday2))
