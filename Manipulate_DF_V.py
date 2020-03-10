3436b80d7c5a7d3460c5ef675ecf84e0db0db414
angelmapmaker <angelmapmaker@gmail.com>
2020-03-10 01:06:43 +0000

    Update Manipulate_DF_V.py

--- Manipulate_DF_V.py
+++ Manipulate_DF_V.py
@@ -20,3 +20,75 @@ medal_counts = country_names.value_counts()
 
 # Print top 15 countries ranked by medals
 print(medal_counts.head(15))
+
+
+########################################################################
+#Using .pivot_table() to count medals by type
+#Rather than ranking countries by total medals won and showing that list, you may want to see a bit more detail. You can use a pivot table to compute how many separate bronze, silver and gold medals each country won. That pivot table can then be used to repeat the previous computation to rank by total medals won.
+#
+#In this exercise, you will use .pivot_table() first to aggregate the total medals by type. Then, you can use .sum() along the columns of the pivot table to produce a new column. When the modified pivot table is sorted by the total medals column, you can display the results from the last exercise with a bit more detail.
+#
+#Instructions
+#100 XP
+#Construct a pivot table counted from the DataFrame medals, aggregating by 'count'. Use 'NOC' as the index, 'Athlete' for the values, and 'Medal' for the columns.
+#Modify the DataFrame counted by adding a column counted['totals']. The new column 'totals' should contain the result of taking the sum along the columns (i.e., use .sum(axis='columns')).
+#Overwrite the DataFrame counted by sorting it with the .sort_values() method. Specify the keyword argument ascending=False.
+#Print the first 15 rows of counted using .head(15). This has been done for you, so hit 'Submit Answer' to see the result
+# Construct the pivot table: counted
+counted = medals.pivot_table(aggfunc='count', index='NOC', columns='Medal', values='Athlete')
+
+# Create the new column: counted['totals']
+counted['totals'] = counted.sum(axis='columns')
+
+# Sort counted by the 'totals' column
+counted = counted.sort_values('totals', ascending=False)
+
+# Print the top 15 rows of counted
+print(counted.head(15))
+
+###########################################################
+#Applying .drop_duplicates()
+#What could be the difference between the 'Event_gender' and 'Gender' columns? You should be able to evaluate your guess by looking at the unique values of the pairs (Event_gender, Gender) in the data. In particular, you should not see something like (Event_gender='M', Gender='Women'). However, you will see that, strangely enough, there is an observation with (Event_gender='W', Gender='Men').
+#
+#The duplicates can be dropped using the .drop_duplicates() method, leaving behind the unique observations. The DataFrame has been loaded as medals.
+#
+#Instructions
+#100 XP
+#Select the columns 'Event_gender' and 'Gender'.
+#Create a dataframe ev_gen_uniques containing the unique pairs contained in ev_gen.
+#Print ev_gen_uniques. This has been done for you, so hit 'Submit Answer' to see the result.
+
+
+# Select columns: ev_gen
+ev_gen = medals.loc[:,['Event_gender', 'Gender']]
+
+# Drop duplicate pairs: ev_gen_uniques
+ev_gen_uniques = ev_gen.drop_duplicates()
+
+# Print ev_gen_uniques
+print(ev_gen_uniques)
+
+
+###########################################################
+#Finding possible errors with .groupby()
+#You will now use .groupby() to continue your exploration. Your job is to group by 'Event_gender' and 'Gender' and count the rows.
+#
+#You will see that there is only one suspicious row: This is likely a data error.
+#
+#The DataFrame is available to you as medals.
+#
+#Instructions
+#100 XP
+#Group medals by 'Event_gender' and 'Gender'.
+#Create a medal_count_by_gender DataFrame with a group count using the .count() method.
+#Print medal_count_by_gender. This has been done for you, so hit 'Submit Answer' to view the result.
+
+# Group medals by the two columns: medals_by_gender
+medals_by_gender = medals.groupby(['Event_gender' , 'Gender'])
+
+# Create a DataFrame with a group count: medal_count_by_gender
+medal_count_by_gender = medals_by_gender.count()
+
+# Print medal_count_by_gender
+print(medal_count_by_gender)
+
