40adcda7a8c8f51bfa413f99b9955cc030a55b05
angelmapmaker <angelmapmaker@gmail.com>
2020-03-11 08:42:34 -0600

    Update Merging_DataFrames_I.py

--- Merging_DataFrames_I.py
+++ Merging_DataFrames_I.py
@@ -231,3 +231,46 @@ temps_c.columns = temps_c.columns.str.replace('F','C')
 # Print first 5 rows of temps_c
 print(temps_c.head())
 
+#########################################
+#Computing percentage growth of GDP
+#Your job in this exercise is to compute the yearly percent-change of US GDP (Gross Domestic Product) since 2008.
+#
+#The data has been obtained from the Federal Reserve Bank of St. Louis and is available in the file GDP.csv, which contains quarterly data; you will resample it to annual sampling and then compute the annual growth of GDP. For a refresher on resampling, check out the relevant material from pandas Foundations.
+#
+#Instructions
+#0 XP
+#Read the file 'GDP.csv' into a DataFrame called gdp, using parse_dates=True and index_col='DATE'.
+#Create a DataFrame post2008 by slicing gdp such that it comprises all rows from 2008 onward.
+#Print the last 8 rows of the slice post2008. This has been done for you. This data has quarterly frequency so the indices are separated by three-month intervals.
+#Create the DataFrame yearly by resampling the slice post2008 by year. Remember, you need to chain .resample() (using the alias 'A' for annual frequency) with some kind of aggregation; you will use the aggregation method .last() to select the last element when resampling.
+#Compute the percentage growth of the resampled DataFrame yearly with .pct_change() * 100.
+#Hint
+#Use pd.read_csv() with 'GDP.csv' as an argument along with the keyword arguments parse_dates=True and index_col='DATE'.
+#Create post2008 by assigning gdp['2008':] to it.
+#Use the .resample() method on post2008 with 'A' as the argument. Then, chain the .last() method to this.
+#Use the .pct_change() method on yearly and multiply this by 100 to compute the percentage growth of the resampled DataFrame.
+
+
+import pandas as pd
+
+# Read 'GDP.csv' into a DataFrame: gdp
+gdp = pd.read_csv('GDP.csv', index_col='DATE', parse_dates=True)
+
+# Slice all the gdp data from 2008 onward: post2008
+post2008 = gdp['2008':]
+
+# Print the last 8 rows of post2008
+print(post2008.tail(8))
+
+# Resample post2008 by year, keeping last(): yearly
+yearly = post2008.resample('A').last()
+
+# Print yearly
+print(yearly)
+
+# Compute percentage growth of yearly: yearly['growth']
+yearly['growth'] = yearly.pct_change() * 100
+
+# Print yearly again
+print(yearly)
+
